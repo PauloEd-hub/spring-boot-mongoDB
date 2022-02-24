53e7e54f0830ecc27800446be8b9ebbb516f5d7b
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-02-24 12:18:51 -0300

    Criação do projeto

---
+++ src/main/java/com/paulocavalcante/mongodb/WorkshopmongoPauloApplication.java
@@ -0,0 +1,13 @@
+package com.paulocavalcante.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoPauloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoPauloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulocavalcante/mongodb/WorkshopmongoPauloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulocavalcante.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoPauloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
